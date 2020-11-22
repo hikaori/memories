72996e27bc9a9c74590f757fcf0992af5ad2e726
hikaori <kaori55194@gmail.com>
2020-11-23 01:06:44 +0900

    add packages for import and modify index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,27 @@
 import express from 'express';
-import bodyParse from 'body-parser';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import nodemon from 'nodemon';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', express: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', express: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  'mongodb+srv://admin:admin1234@cluster0.rn2bt.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`),
+    ),
+  )
+  .catch(error => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
