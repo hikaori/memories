69f1e6b56227585fc5d65068bb69dc8b3a031e6a
hikaori <kaori55194@gmail.com>
2020-11-23 16:13:32 +0900

    controller and route of create post

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -10,6 +10,14 @@ export const getPosts = async (req, res) => {
   }
 };
 
-export const createPosts = (request, res) => {
-  res.send('Post Creation!');
+export const createPosts = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error });
+  }
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
-import { getPosts, createPost } from '../controller/posts.js';
+import { getPosts, createPosts } from '../controller/posts.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
-// router.post('/', createPost);
+router.post('/', createPosts);
 
 export default router;
