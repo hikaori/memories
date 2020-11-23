711a73748871facd3c65bde970044fbbd201f81a
hikaori <kaori55194@gmail.com>
2020-11-23 16:03:43 +0900

    connect server

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -1,3 +1,15 @@
-export const getPosts = (request, response) => {
-  response.send('this works!');
+import PostMessage from '../models/postMessages.js';
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPosts = (request, res) => {
+  res.send('Post Creation!');
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { getPosts } from '../container/posts.js';
+import { getPosts, createPost } from '../controller/posts.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
+// router.post('/', createPost);
 
 export default router;
